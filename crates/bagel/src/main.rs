0da47298fe3559818d6ea906a753673262dfae97
Jens Meindertsma <mail@jensmeindertsma.com>
2025-05-13 17:09:56 +0200

    work:

--- crates/bagel/src/main.rs
+++ crates/bagel/src/main.rs
@@ -1,10 +1,12 @@
 mod codecrafters;
 mod colors;
+mod scanner;
 
 use codecrafters::is_codecrafters_environment;
 use colors::Colorization;
 use core::fmt::{self, Formatter};
 use owo_colors::OwoColorize;
+use scanner::{Scanner, ScannerError};
 use std::{
     env,
     fs::{self, File},
@@ -17,19 +19,24 @@ fn main() -> impl Termination {
     let colorization = Colorization::determine();
 
     if let Err(failure) = run(colorization) {
-        // TODO: based on is_codecrafters, print errors differently to pass their tests
-        if is_codecrafters_environment() {
-            eprintln!("TODO codecrafters error")
-        } else {
-            match colorization {
-                Colorization::Disabled => eprintln!("error: {failure}"),
-                Colorization::Enabled => {
-                    eprintln!("{}{} {failure}", "error".bold().red(), ":".bold())
+        match failure {
+            Failure::Scanner(errors) => {
+                if is_codecrafters_environment() {
+                    for error in errors {
+                        match error {
+                            ScannerError::UnexpectedCharacter { character, line } => {
+                                eprintln!("[line {line}] Error: Unexpected character: {character}")
+                            }
+                        }
+                    }
+                } else {
+                    print_failure(Failure::Scanner(errors), colorization);
                 }
             }
-        };
+            failure => print_failure(failure, colorization),
+        }
 
-        failure.exit_code()
+        ExitCode::FAILURE
     } else {
         ExitCode::SUCCESS
     }
@@ -44,9 +51,9 @@ fn run(colorization: Colorization) -> Result<(), Failure> {
     // the first argument is the binary which we can ignore
     let mut arguments = env::args().skip(1);
 
-    let command = arguments
-        .next()
-        .ok_or(Failure::MissingArgument("command"))?;
+    let Some(command) = arguments.next() else {
+        todo!("implement REPL");
+    };
 
     match command.as_str() {
         "help" => {
@@ -72,9 +79,22 @@ fn run(colorization: Colorization) -> Result<(), Failure> {
         "tokenize" => {
             let path = arguments.next().ok_or(Failure::MissingArgument("file"))?;
 
-            let _contents = fs::read_to_string(path).map_err(Failure::FileRead)?;
+            let contents =
+                fs::read_to_string(&path).map_err(|error| Failure::FileRead { path, error })?;
 
-            todo!()
+            let scanner = Scanner::new(&contents);
+            let mut errors = Vec::new();
+
+            for result in scanner {
+                match result {
+                    Ok(token) => println!("{token}"),
+                    Err(error) => errors.push(error),
+                }
+            }
+
+            if !errors.is_empty() {
+                return Err(Failure::Scanner(errors));
+            }
         }
         _ => return Err(Failure::UnknownCommand(command)),
     }
@@ -84,9 +104,10 @@ fn run(colorization: Colorization) -> Result<(), Failure> {
 
 #[derive(Debug)]
 enum Failure {
-    FileRead(io::Error),
+    FileRead { path: String, error: io::Error },
     LogFileCreation(io::Error),
     MissingArgument(&'static str),
+    Scanner(Vec<ScannerError>),
     UnknownCommand(String),
 }
 
@@ -99,20 +120,41 @@ impl Failure {
 impl fmt::Display for Failure {
     fn fmt(&self, formatter: &mut Formatter<'_>) -> fmt::Result {
         match self {
-            Self::FileRead(io_error) => write!(formatter, "failed to read source file: {io_error}"),
+            Self::FileRead { path, error } => {
+                write!(formatter, "failed to read source file `{path}`: {error}")
+            }
+
             Self::LogFileCreation(io_error) => write!(
                 formatter,
                 "failed to create log file at `/tmp/bagel.log`: {io_error}"
             ),
+
             Self::MissingArgument(argument) => write!(
                 formatter,
                 "missing required argument `{argument}`, run `bagel help` for assistence"
             ),
 
+            Self::Scanner(errors) => {
+                for error in errors {
+                    writeln!(formatter, "{error}")?;
+                }
+
+                Ok(())
+            }
+
             Self::UnknownCommand(command) => write!(
                 formatter,
                 "unknown command `{command}`, run `bagel help` for assistence"
             ),
+        }
+    }
+}
+
+fn print_failure(failure: Failure, colorization: Colorization) {
+    match colorization {
+        Colorization::Disabled => eprintln!("error: {failure}"),
+        Colorization::Enabled => {
+            eprintln!("{}{} {failure}", "error".bold().red(), ":".bold())
         }
     }
 }
---
+++ crates/bagel/src/scanner.rs
@@ -0,0 +1,140 @@
+use core::fmt::{self, Formatter};
+use std::{error::Error, iter::Peekable, str::Chars};
+
+#[derive(Debug, Clone)]
+pub struct Scanner<'a> {
+    characters: Peekable<Chars<'a>>,
+    current_line: usize,
+    done: bool,
+}
+
+impl<'a> Scanner<'a> {
+    pub fn new(input: &'a str) -> Self {
+        Self {
+            characters: input.chars().peekable(),
+            current_line: 1,
+            done: false,
+        }
+    }
+
+    fn produce(&self, kind: TokenKind) -> Option<Result<Token, ScannerError>> {
+        Some(Ok(Token {
+            kind,
+            line: self.current_line,
+        }))
+    }
+
+    fn fail(
+        &self,
+        closure: impl FnOnce(usize) -> ScannerError,
+    ) -> Option<Result<Token, ScannerError>> {
+        let error = closure(self.current_line);
+
+        Some(Err(error))
+    }
+}
+
+impl Iterator for Scanner<'_> {
+    type Item = Result<Token, ScannerError>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.characters.next() {
+            None => {
+                if self.done {
+                    tracing::trace!("`scanner.next()` was called again despite already being done");
+
+                    None
+                } else {
+                    tracing::debug!("finished processing input, returning end of file token");
+
+                    self.done = true;
+                    Some(Ok(Token {
+                        kind: TokenKind::EndOfFile,
+                        line: self.current_line,
+                    }))
+                }
+            }
+            Some(character) => {
+                tracing::trace!("processing next character `{character}`");
+
+                let token_kind = match character {
+                    '\n' => {
+                        self.current_line += 1;
+                        return self.next();
+                    }
+                    '(' => TokenKind::LeftParenthesis,
+                    ')' => TokenKind::RightParenthesis,
+                    _ => {
+                        return self
+                            .fail(|line| ScannerError::UnexpectedCharacter { character, line });
+                    }
+                };
+
+                tracing::trace!("producing new token `{token_kind:?}`");
+
+                self.produce(token_kind)
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Token {
+    pub kind: TokenKind,
+    pub line: usize,
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, formatter: &mut Formatter<'_>) -> std::fmt::Result {
+        // Defer formatting to `TokenKind`
+        write!(formatter, "{}", self.kind)
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum TokenKind {
+    EndOfFile,
+    LeftParenthesis,
+    RightParenthesis,
+}
+
+impl fmt::Display for TokenKind {
+    fn fmt(&self, formatter: &mut Formatter<'_>) -> std::fmt::Result {
+        // We format the enum variants into a standard format defined in the book:
+        // <token_type> <lexeme> <literal>
+        //
+        // <token_type>: The type of the token.
+        // Examples: VAR, IDENTIFIER, STRING, EOF etc.
+        //
+        // <lexeme>: The actual sequence of characters that formed the token.
+        // Examples: var, breakfast, "bagels" etc.
+        // For an EOF token, the lexeme is an empty string.
+        //
+        // <literal>: The literal value of the token.
+        // For most tokens this is null.
+        // For STRING/NUMBER tokens, it holds the value of the string/number.
+
+        match self {
+            Self::EndOfFile => write!(formatter, "EOF  null"),
+            _ => todo!(),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum ScannerError {
+    UnexpectedCharacter { character: char, line: usize },
+}
+
+impl fmt::Display for ScannerError {
+    fn fmt(&self, formatter: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::UnexpectedCharacter { character, line } => write!(
+                formatter,
+                "unexpected character `{character}` on line {line}"
+            ),
+        }
+    }
+}
+
+impl Error for ScannerError {}
