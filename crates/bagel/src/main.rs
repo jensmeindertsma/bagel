68bb0bed190477d5bd7ae94c3d7a3da4d5f80897
Jens Meindertsma <mail@jensmeindertsma.com>
2025-05-31 19:44:20 +0200

    rename

--- crates/bagel/src/main.rs
+++ crates/bagel/src/main.rs
@@ -4,6 +4,6 @@ use tracing::Level;
 fn main() -> impl Termination {
     tracing_subscriber::fmt()
         .with_max_level(Level::TRACE)
-        .with_writer(File::create("/tmp/bagel.log").expect("writer should be initialized"))
+        .with_writer(File::create("/tmp/bagel.log").expect("file creation should succeed"))
         .init();
 }
